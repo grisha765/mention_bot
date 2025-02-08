846dfaf9ab734c8fda6236c59104cf0847a52bc4
Grisha <grisha765@tuta.io>
2025-02-08 19:03:03 +0300

    [f] fix filter

--- func/filters.py
+++ func/filters.py
@@ -3,10 +3,15 @@ from db.setting import get_setting
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.types import Message, CallbackQuery
 
-async def is_all_access(_, __, message) -> bool:
+async def is_all_access(_, client, message) -> bool:
     chat_id = message.chat.id
+    user = message.from_user
     all_access = await get_setting(chat_id, "all_access")
-    return all_access
+    if not all_access:
+        member = await client.get_chat_member(chat_id, user.id)
+        if member.status not in (ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR):
+            return False
+    return True
 
 async def is_admin(_, client, update) -> bool:
     if isinstance(update, Message):
